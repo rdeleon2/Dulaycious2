39162bae5dbeb5bc14d38427000bd0bfc37c5770
Rdeleon <rdeleonjr1007@yahoo.com>
2020-06-10 13:11:12 -0700

    images

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import Menu from './Menu';
+
 function Home (props){
     return(
         <div className="container">
             <h1>HomePage</h1>
             <Link to="/menu">MENU</Link>
-            <Menu />
         </div>
     )
 }
